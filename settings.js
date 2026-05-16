806b93a6fe767ca11191ff057bc4a9185d04b8cc
˚₊· ͟͟͞͞  ɪ ᴀᴍ ᴋᴀᴍᴇᴋɪ <zoredevteam@gmail.com>
2026-05-15 22:27:16 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ global.rcanal         = 'https://whatsapp.com/channel/0029Vb85bh7EAKWOM4Zw8N3G'
 global.newsletterJid  = '120363408182996815@newsletter'
 global.newsletterName = '༺𝕿𝔧᭄⏤͟͟͞͞𝕿𝖔𝖏𝖎 𝕱𝖚𝖘𝖍𝖎𝖌𝖚𝖗𝖔𒆜℘࿐༵'
 
-global.banner = 'https://files.catbox.moe/ktolbv.png'
+global.banner = 'https://upload.yotsuba.giize.com/u/Uj1apzmd.jpg'
 global.icono  = 'https://i.pinimg.com/564x/6e/2b/2e/6e2b2e5e1e6e2b2e5e1e6e2b2e5e1e6e.jpg'
 
 // ── Iconos aleatorios de Toji ────────────────────────────
