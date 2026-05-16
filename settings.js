98eb439a43de9b56ac98d0321a3edab6be8e88be
˚₊· ͟͟͞͞  ɪ ᴀᴍ ᴋᴀᴍᴇᴋɪ <zoredevteam@gmail.com>
2026-05-16 01:00:16 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -45,7 +45,7 @@ global.iconosToji = [
   'https://upload.yotsuba.giize.com/u/l6OnzK9q.png',
   'https://upload.yotsuba.giize.com/u/Tv2DigZL.png',
   'https://upload.yotsuba.giize.com/u/h8B9BZl2.png',
-  'https://i.pinimg.com/736x/c9/d0/e1/c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4.jpg',
+  'https://upload.yotsuba.giize.com/u/piI2v8gs.png',
   'https://i.pinimg.com/736x/d3/e4/f5/d3e4f5a6b7c8d9e0f1a2b3c4d5e6f7a8.jpg',
   'https://i.pinimg.com/736x/e7/f8/a9/e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2.jpg',
   'https://i.pinimg.com/736x/f0/a1/b2/f0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5.jpg'
