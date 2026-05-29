481402c98378992e00fedc1baca9f2cc0ba3f407
˚₊· ͟͟͞͞  ɪ ᴀᴍ ᴋᴀᴍᴇᴋɪ <zoredevteam@gmail.com>
2026-05-28 19:08:49 -0500

    Crear menú.js

---
+++ menú.js
@@ -0,0 +1,177 @@
+*╭╼𝅄꒰𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪ ꒱ 𐔌 BOT - INFO 𐦯*
+*|✎ Creador:* 𓆩‌۫᷼ ִֶָღܾ݉͢ғ꯭ᴇ꯭፝ℓɪ꯭ͨא𓆪
+*|✎ Categorías:* 14
+*|✎ Baileys:* PixelCrew-Bails
+*|✎ Comandos:* https://yotsuba-web.giize.com/commands.html
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬👑⃘۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*
+
+*➪ 𝗟𝗜𝗦𝗧𝗔*
+       *➪  𝗗𝗘*
+           *➪ 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦*
+`;
+
+const dev = `
+> 𝓓𝓮𝓿𝓮𝓵𝓸𝓹𝓮𝓭 𝓫𝔂 𝗗𝙚𝙮𝙢𝙤𝙤𝙣𝗢𝙛𝙘 ❤️
+`;
+
+const menuConfig = [
+  { cmd: 'menufreefire', name: 'FREE-FIRE', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 FREE-FIRE 𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #purgatorio*
+> *𑁍⃪࣭۪ٜ ❏ #kalahari*
+> *𑁍⃪࣭۪ٜ ❏ #alpes*
+> *𑁍⃪࣭۪ٜ ❏ #bermuda*
+> *𑁍⃪࣭۪ٜ ❏ #formarsala*
+*╰─ׅ─ׅ┈ ─๋︩︪─☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬☃️⃘۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪─╯*` },
+  { cmd: 'menusystem', name: 'SISTEMA', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 SISTEMA 𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #p*
+> *𑁍⃪࣭۪ٜ ❏ #ping*
+> *𑁍⃪࣭۪ٜ ❏ #menu*
+> *𑁍⃪࣭۪ٜ ❏ #help*
+> *𑁍⃪࣭۪ٜ ❏ #owner*
+*╰─ׅ─ׅ┈ ─๋︩︪─☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬☃️⃘۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪─╯*` },
+  { cmd: 'menuherramientas', name: 'HERRAMIENTAS', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 HERRAMIENTAS 𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #pinterest <texto>*
+> *𑁍⃪࣭۪ٜ ❏ #catbox <imagen>*
+> *𑁍⃪࣭۪ٜ ❏ #toimg <sticker>*
+> *𑁍⃪࣭۪ٜ ❏ #pin <texto>*
+*╰─ׅ─ׅ┈ ─๋︩︪─☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬☃️⃘۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪─╯*` },
+  { cmd: 'menudescargas', name: 'DESCARGAS', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 DESCARGAS 𐦯
+> *𑁍⃪࣭۪ٜ ❏ #apkmod <texto>*
+> *𑁍⃪࣭۪ٜ ❏ #apk*
+> *𑁍⃪࣭۪ٜ ❏ #ig <enlace>*
+> *𑁍⃪࣭۪ٜ ❏ #instagram*
+> *𑁍⃪࣭۪ٜ ❏ #fb <enlace>*
+> *𑁍⃪࣭۪ٜ ❏ #facebook*
+> *𑁍⃪࣭۪ٜ ❏ #tt <enlace>*
+> *𑁍⃪࣭۪ٜ ❏ #tiktok*
+> *𑁍⃪࣭۪ٜ ❏ #mf <enlace>*
+> *𑁍⃪࣭۪ٜ ❏ #mediafire*
+> *𑁍⃪࣭۪ٜ ❏ #play <musica>*
+> *𑁍⃪࣭۪ٜ ❏ #yts*
+> *𑁍⃪࣭۪ٜ ❏ #ytv*
+> *𑁍⃪࣭۪ٜ ❏ #play2*
+> *𑁍⃪࣭۪ٜ ❏ #ytm3*
+> *𑁍⃪࣭۪ٜ ❏ #ytmp4*
+> *𑁍⃪࣭۪ٜ ❏ #yta*
+*╰─ׅ─ׅ┈ ─๋︩︪─☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬☃️⃘۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪─╯*` },
+  { cmd: 'menusockets', name: 'SOCKETS', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 SOCKETS  𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #qr*
+> *𑁍⃪࣭۪ٜ ❏ #code*
+> *𑁍⃪࣭۪ٜ ❏ #self <on/off>*
+> *𑁍⃪࣭۪ٜ ❏ #sologp <on/off>*
+> *𑁍⃪࣭۪ٜ ❏ #logout*
+> *𑁍⃪࣭۪ٜ ❏ #leave*
+> *𑁍⃪࣭۪ٜ ❏ #setname <nombre>*
+> *𑁍⃪࣭۪ٜ ❏ #setbanner <foto>*
+> *𑁍⃪࣭۪ٜ ❏ #setcurrency <moneda>*
+> *𑁍⃪࣭۪ٜ ❏ #setmoneda <moneda>*
+> *𑁍⃪࣭۪ٜ ❏ #set*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪⃘⃪𐇽۪۫۬֟፝֯۫۫۫۬🔔◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+  { cmd: 'menuassistant', name: 'ASISTANT', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 ASISTANT 𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #ai*
+> *𑁍⃪࣭۪ٜ ❏ #bard*
+> *𑁍⃪࣭۪ٜ ❏ #chatgpt*
+> *𑁍⃪࣭۪ٜ ❏ #dalle*
+> *𑁍⃪࣭۪ٜ ❏ #flux*
+> *𑁍⃪࣭۪ٜ ❏ #gemini*
+> *𑁍⃪࣭۪ٜ ❏ #ia*
+> *𑁍⃪࣭۪ٜ ❏ #iavoz*
+> *𑁍⃪࣭۪ٜ ❏ #luminai*
+> *𑁍⃪࣭۪ٜ ❏ #openai*
+> *𑁍⃪࣭۪ٜ ❏ #yotsuba*
+> *𑁍⃪࣭۪ٜ ❏ #yotsuba-nakano-ia*
+*╰─ׅ─ׅ┈ ─๋︩︪─☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬☃️⃘۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪─╯*` },
+  { cmd: 'menujuegos', name: 'JUEGOS', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 JUEGOS  𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #formarpareja5*
+> *𑁍⃪࣭۪ٜ ❏ #formarpareja*
+> *𑁍⃪࣭۪ٜ ❏ #top*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬❄◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+  { cmd: 'menugestion', name: 'GESTIÓN', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 GESTIÓN 𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #testwelcome
+> *𑁍⃪࣭۪ٜ ❏ #testbye
+> *𑁍⃪࣭۪ٜ ❏ #bye <on/off>*
+> *𑁍⃪࣭۪ٜ ❏ #welcome <on/off>*
+> *𑁍⃪࣭۪ٜ ❏ #antienlace <on/off>*
+> *𑁍⃪࣭۪ٜ ❏ #antilink <on/off>*
+> *𑁍⃪࣭۪ٜ ❏ #modoadmin <on/off>* 
+> *𑁍⃪࣭۪ٜ ❏ #detect <on/off>* 
+> *𑁍⃪࣭۪ٜ ❏ #open*
+> *𑁍⃪࣭۪ٜ ❏ #close*
+> *𑁍⃪࣭۪ٜ ❏ #abrir*
+> *𑁍⃪࣭۪ٜ ❏ #cerrar*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬🔔◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+  { cmd: 'menurpg', name: 'RPG', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 RPG  𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #daily*
+> *𑁍⃪࣭۪ٜ ❏ #cofre*
+> *𑁍⃪࣭۪ٜ ❏ #minar*
+> *𑁍⃪࣭۪ٜ ❏ #rob*
+> *𑁍⃪࣭۪ٜ ❏ #rob2*
+> *𑁍⃪࣭۪ٜ ❏ #depositar <all>*
+> *𑁍⃪࣭۪ٜ ❏ #d <all>*
+> *𑁍⃪࣭۪ٜ ❏ #lvl*
+> *𑁍⃪࣭۪ٜ ❏ #bal*
+> *𑁍⃪࣭۪ٜ ❏ #baltop*
+> *𑁍⃪࣭۪ٜ ❏ #w*
+> *𑁍⃪࣭۪ٜ ❏ #trabajar*
+> *𑁍⃪࣭۪ٜ ❏ #work*
+> *𑁍⃪࣭۪ٜ ❏ #chambear*
+> *𑁍⃪࣭۪ٜ ❏ #chamba*
+> *𑁍⃪࣭۪ٜ ❏ #slut*
+> *𑁍⃪࣭۪ٜ ❏ #prostituirse*
+> *𑁍⃪࣭۪ٜ ❏ #perfil*
+> *𑁍⃪࣭۪ٜ ❏ #profile*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬🎄◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+  { cmd: 'menugrupos', name: 'GRUPOS', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 GRUPOS 𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #demote*
+> *𑁍⃪࣭۪ٜ ❏ #promote*
+> *𑁍⃪࣭۪ٜ ❏ #delete*
+> *𑁍⃪࣭۪ٜ ❏ #kick*
+> *𑁍⃪࣭۪ٜ ❏ #del*
+> *𑁍⃪࣭۪ٜ ❏ #promover*
+> *𑁍⃪࣭۪ٜ ❏ #degradar*
+> *𑁍⃪࣭۪ٜ ❏ #delprimary*
+> *𑁍⃪࣭۪ٜ ❏ #setprimary*
+> *𑁍⃪࣭۪ٜ ❏ #tagall*
+> *𑁍⃪࣭۪ٜ ❏ #invocar*
+> *𑁍⃪࣭۪ٜ ❏ #todos*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬☃️◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+  { cmd: 'menuowner', name: 'OWNER', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 OWNER  𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #autoadmin*
+> *𑁍⃪࣭۪ٜ ❏ #join*
+> *𑁍⃪࣭۪ٜ ❏ #update*
+> *𑁍⃪࣭۪ٜ ❏ #spamwa*
+> *𑁍⃪࣭۪ٜ ❏ #prefix*
+> *𑁍⃪࣭۪ٜ ❏ #rprefix*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬❄⃘⃪۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+  { cmd: 'menunsfw', name: 'NSFW', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 NSFW  𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #sexo*
+> *𑁍⃪࣭۪ٜ ❏ #69*
+> *𑁍⃪࣭۪ٜ ❏ #violar*
+> *𑁍⃪࣭۪ٜ ❏ #r34*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬🎁⃘۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+  { cmd: 'menuemox', name: 'EMOX', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 EMOX  𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #bailar*
+> *𑁍⃪࣭۪ٜ ❏ #dance*
+> *𑁍⃪࣭۪ٜ ❏ #lamer*
+> *𑁍⃪࣭۪ٜ ❏ #lamber*
+> *𑁍⃪࣭۪ٜ ❏ #feliz*
+> *𑁍⃪࣭۪ٜ ❏ #happy*
+> *𑁍⃪࣭۪ٜ ❏ #triste*
+> *𑁍⃪࣭۪ٜ ❏ #borracho*
+> *𑁍⃪࣭۪ٜ ❏ #drunk*
+> *𑁍⃪࣭۪ٜ ❏ #kill*
+> *𑁍⃪࣭۪ٜ ❏ #matar*
+> *𑁍⃪࣭۪ٜ ❏ #kiss*
+> *𑁍⃪࣭۪ٜ ❏ #besar*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬☃️⃘⃪۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+  { cmd: 'menustickers', name: 'STICKERS', text: `*꒰⌢◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸ ✎ ꒱ 𐔌 STICKERS 𐦯*
+> *𑁍⃪࣭۪ٜ ❏ #s*
+> *𑁍⃪࣭۪ٜ ❏ #sticker*
+> *𑁍⃪࣭۪ٜ ❏ #brat*
+> *𑁍⃪࣭۪ٜ ❏ #qc*
+> *𑁍⃪࣭۪ٜ ❏ #emojimix*
+> *𑁍⃪࣭۪ٜ ❏ #take*
+> *𑁍⃪࣭۪ٜ ❏ #wm*
+> *𑁍⃪࣭۪ٜ ❏ #bratv*
+*╰─ׅ─ׅ┈─๋︩︪─☪︎︎︎̸⃘࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬🌟◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈─๋︩︪─╯*` },
+]
